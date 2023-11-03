e404a4bd77240e484ec2d1c3e5362763497348a5
Jace <2837363L@student.gla.ac.uk>
2023-11-03 08:02:15 +0800

    Refactor line and ultra codes
    [+] compartmentalized them into functions and cleaning out main()

--- driver/irline/Line/irsensor.c
+++ driver/irline/Line/irsensor.c
@@ -2,25 +2,43 @@
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 
-// Define the GPIO pin connected to the IR line sensor
-#define IR_SENSOR_PIN 26
+#define IR_SENSOR1_PIN 26
+#define IR_SENSOR2_PIN 27
+
+// Function to check if an IR sensor detects a line
+bool isLineDetected(uint gpio_pin) {
+    return gpio_get(gpio_pin);
+}
+
+// Function to handle the car's behavior based on sensor readings
+void handleCarBehavior() {
+    if (isLineDetected(IR_SENSOR1_PIN)) {
+        printf("Line detected by IR Sensor 1\n");
+        // Implement behavior for Sensor 1 detection
+    }
+
+    if (isLineDetected(IR_SENSOR2_PIN)) {
+        printf("Line detected by IR Sensor 2\n");
+        // Implement behavior for Sensor 2 detection
+    }
+
+    // Implement complex behavior here later
+    // using if-else or a state machine for more complex behavior
+}
 
 int main() {
-    stdio_init_all(); // Initialize serial communication for debugging
+    stdio_init_all();
+    
+    gpio_init(IR_SENSOR1_PIN);
+    gpio_set_dir(IR_SENSOR1_PIN, GPIO_IN);
 
-    // Initialize the GPIO pin for the IR sensor as an input
-    gpio_init(IR_SENSOR_PIN);
-    gpio_set_dir(IR_SENSOR_PIN, GPIO_IN);
+    gpio_init(IR_SENSOR2_PIN);
+    gpio_set_dir(IR_SENSOR2_PIN, GPIO_IN);
 
     while (1) {
-        // Read the value of the IR sensor
-        bool lineDetected = gpio_get(IR_SENSOR_PIN);
+        handleCarBehavior();
 
-        if (lineDetected) {
-            printf("Detected a black line!\n");
-        }
-
-        sleep_ms(100); // Adjust the sleep time as needed
+        sleep_ms(100);
     }
 
     return 0;
--- driver/ultrasonic/ultrasonic.c
+++ driver/ultrasonic/ultrasonic.c
@@ -4,7 +4,6 @@
 #include "hardware/gpio.h"
 #include "hardware/irq.h"
 
-// Define the trigger and echo pins
 #define TRIGGER_PIN 19
 #define ECHO_PIN 18
 #define SPEED_OF_SOUND_CM_US 0.0343
@@ -12,26 +11,20 @@
 #define OBJECT_DETECTION_THRESHOLD 6.0
 #define NUM_AVERAGES 8
 
-// Global variables for object detection
 volatile bool object_detected = false;
 volatile float latest_distance_cm = 0.0;
 
-// Callback function for the echo interrupt
 void echo_interrupt_handler() {
-    // Check if the echo pin is high (object detected) and distance is below the threshold
     if (gpio_get(ECHO_PIN) && latest_distance_cm <= OBJECT_DETECTION_THRESHOLD) {
         object_detected = true;
     }
 }
 
-// Measure distance using the ultrasonic sensor
 float measure_distance() {
-    // Trigger the sensor by setting the trigger pin high and low
     gpio_put(TRIGGER_PIN, 1);
     sleep_us(10);
     gpio_put(TRIGGER_PIN, 0);
 
-    // Measure the time it takes for the echo pulse to return
     uint32_t start_time = time_us_32();
     while (gpio_get(ECHO_PIN) == 0 && time_us_32() - start_time < MAX_WAIT_TIME_US);
     start_time = time_us_32();
@@ -39,13 +32,11 @@ float measure_distance() {
     while (gpio_get(ECHO_PIN) == 1 && time_us_32() - start_time < MAX_WAIT_TIME_US);
     uint32_t end_time = time_us_32();
 
-    // Calculate the distance from the duration of the echo pulse
     uint32_t duration = end_time - start_time;
     float distance = (float)duration * SPEED_OF_SOUND_CM_US / 2.0;
     return distance;
 }
 
-// Calculate the average of an array of distances
 float calculate_average(float distances[], int size) {
     float total = 0.0;
     for (int i = 0; i < size; i++) {
@@ -54,34 +45,24 @@ float calculate_average(float distances[], int size) {
     return total / size;
 }
 
-int main() {
-    stdio_init_all();
+void perform_distance_measurement(float raw_distances[], int* average_counter) {
+    for (int i = 0; i < NUM_AVERAGES; i++) {
+        float distance = measure_distance();
+        raw_distances[i] = distance;
+    }
+    *average_counter = (*average_counter + 1) % NUM_AVERAGES;
+}
 
-    // Initialize the trigger and echo pins
-    gpio_init(TRIGGER_PIN);
-    gpio_set_dir(TRIGGER_PIN, GPIO_OUT);
-
-    gpio_init(ECHO_PIN);
-    gpio_set_dir(ECHO_PIN, GPIO_IN);
-
-    // Enable an interrupt on the echo pin for rising edge events
-    gpio_set_irq_enabled_with_callback(ECHO_PIN, GPIO_IRQ_EDGE_RISE, true, echo_interrupt_handler);
-
+void check_and_print_object_detection() {
     float raw_distances[NUM_AVERAGES];
     int average_counter = 0;
 
     while (1) {
-        // Collect multiple distance measurements
-        for (int i = 0; i < NUM_AVERAGES; i++) {
-            float distance = measure_distance();
-            raw_distances[i] = distance;
-        }
+        perform_distance_measurement(raw_distances, &average_counter);
 
-        // Calculate the average distance from the measurements
         float average_distance = calculate_average(raw_distances, NUM_AVERAGES);
         latest_distance_cm = average_distance;
 
-        // Check if an object is detected based on the threshold
         if (latest_distance_cm <= OBJECT_DETECTION_THRESHOLD) {
             object_detected = true;
             printf("Object detected\n");
@@ -90,11 +71,22 @@ int main() {
             object_detected = false;
         }
 
-        // Delay before the next measurement
         sleep_ms(1);
-
-        average_counter = (average_counter + 1) % NUM_AVERAGES;
     }
+}
+
+int main() {
+    stdio_init_all();
+
+    gpio_init(TRIGGER_PIN);
+    gpio_set_dir(TRIGGER_PIN, GPIO_OUT);
+
+    gpio_init(ECHO_PIN);
+    gpio_set_dir(ECHO_PIN, GPIO_IN);
+
+    gpio_set_irq_enabled_with_callback(ECHO_PIN, GPIO_IRQ_EDGE_RISE, true, echo_interrupt_handler);
+
+    check_and_print_object_detection();
 
     return 0;
 }
